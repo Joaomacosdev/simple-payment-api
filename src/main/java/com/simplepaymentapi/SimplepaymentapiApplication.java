9a94da44d1211b5c6ee962479df6c525ae3c0c6e
Joaomacosdev <joamarcosdev@gmail.com>
2025-07-23 16:24:39 -0300

    Iniciando o projeto

---
+++ src/main/java/com/simplepaymentapi/SimplepaymentapiApplication.java
@@ -0,0 +1,13 @@
+package com.simplepaymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplepaymentapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplepaymentapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplepaymentapi/SimplepaymentapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplepaymentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplepaymentapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
